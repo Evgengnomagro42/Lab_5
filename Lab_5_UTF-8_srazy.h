09a301ffd6f409f7e7a4fe8a3f30b5e284c5ce4f
Evgengnomagro42 <gnom610004@list.ru>
2025-11-08 16:43:14 +0300

    Update Lab_5_UTF-8_srazy.h

--- Lab_5_UTF-8_srazy.h
+++ Lab_5_UTF-8_srazy.h
@@ -14,7 +14,9 @@ void preobr_digit_letter();
 void zamena_simvola();
 //6
 void vvod_strok1();
-void vvod_strok1();
+void vvod_strok2();
+void sort_alfav(std::string& strings);
+void sort_vozr(std::string &strings);
 //7
 //для остальных типов данных перевод в строку по этому же образцу
 void to_string_i();
@@ -24,6 +26,10 @@ void some_text();
 //8
 wstring Parse(wstring& x);
 wstring dlia_Country(wstring x);
+wstring dlia_Region(wstring x);
+wstring dlia_City(wstring x);
+wstring dlia_Street(wstring x);
+wstring dlia_Hous(wstring x);
 void Unify_print_struct(std::wstring x);
 void Unify_print_bez_struct(wstring x);
 
